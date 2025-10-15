80b5a3cff2673c37b49ed94ba4862e0bc939e0f3
MrNobody23-cloude <aaryanpatel080@gmail.com>
2025-10-15 22:12:18 +0530

    `Update Firebase configuration to use environment variables and add analytics initialization`

--- Frontend/src/firebase/config.js
+++ Frontend/src/firebase/config.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDrejX0h-iiyzQrWJUs0mWjhwKfS7eym3Q",
-  authDomain: "equiphealth23.firebaseapp.com",
-  projectId: "equiphealth23",
-  storageBucket: "equiphealth23.firebasestorage.app",
-  messagingSenderId: "552665705103",
-  appId: "1:552665705103:web:62b5ceb3d611696d80defa",
-  measurementId: "G-QVFR7P3T53"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
