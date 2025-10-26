28aee0fb123ef3279cea495ae917a9bfcb7580f7
MrNobody23-cloude <aaryanpatel080@gmail.com>
2025-10-26 11:42:26 +0530

    fixed server for routing

--- Frontend/src/components/ServiceLocator.jsx
+++ Frontend/src/components/ServiceLocator.jsx
@@ -57,7 +57,7 @@ const ServiceLocator = ({ equipmentList }) => {
 
       // Create script element
       const script = document.createElement('script');
-      script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places`;
+      script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places&loading=async`;
       script.async = true;
       script.defer = true;
 
