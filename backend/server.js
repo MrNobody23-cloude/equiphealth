dfe195b1be7a743339f05c4d4546047b42ad1ff2
MrNobody23-cloude <aaryanpatel080@gmail.com>
2025-10-26 10:36:36 +0530

    fixed server for routing

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const passport = require('passport');
 const cookieParser = require('cookie-parser');
 const connectDB = require('./config/database');
 require('./config/passport');
-const emailService = require('./config/email');
+const emailService = require('./config/gmail');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
